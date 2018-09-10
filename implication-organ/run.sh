db4d9025b9b293d5923224586a3a82485af14c1b
DaveSeidel <dave.seidel@gmail.com>
2018-09-09 21:03:40 -0400

    fix path in script

--- implication-organ/run.sh
+++ implication-organ/run.sh
@@ -3,4 +3,4 @@
 # -Q must use be a Launch Control XL
 # -o will be the Pisound -- change to "-odac" for the generic case
 # csound -Q$(../scripts/get-midi-out) -o $(../scripts/get-dac) implication-organ.csd
-csound -o $(../scripts/get-dac) implication-organ.csd
+csound -o $(./get-dac) implication-organ.csd
